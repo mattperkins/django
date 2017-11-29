2c3015ee7823f80e9bbacd1e4b3cf03871930853
mattperkins <github@mattperkins.com>
2017-11-29 01:26:17 +0000

    add music template detail

--- website1/music/views.py
+++ website1/music/views.py
@@ -1,24 +1,24 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
+from django.shortcuts import render 
 from . models import Album
 
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template('music/index.html')
-    context = {
-        'all_albums' : all_albums,
-    }
-    # ^^^^ see articles/views.py for the simple render method for returning a template
-     
+    return render(request, 'music/index.html', {'all_albums' : all_albums})
      
     # html = ''
     # for album in all_albums:
     #     url = '/music/' +  str(album.id) + '/'
     #     html += '<a href=" ' + url + ' ">' + album.album_title + '</a><br>'
     
-    return HttpResponse(template.render(context, request))
 
 
 def detail(request, album_id):
-    return HttpResponse('<h2>Details for Album id: ' + str(album_id) + '</h2>')
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404('Album does not exist')
+    return render(request, 'music/detail.html', {'album' : album}) 
+    
+    # return HttpResponse('<h2>Details for Album id: ' + str(album_id) + '</h2>')
 
