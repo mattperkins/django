e62e48257399b1d58c2d920029619b56617862a5
mattperkins <github@mattperkins.com>
2017-12-06 02:29:48 +0000

    configure static files

--- website1/blog/urls.py
+++ website1/blog/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -10,3 +11,5 @@ urlpatterns = [
     url(r'^about/$', views.about),
     url(r'^$', views.homepage),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
